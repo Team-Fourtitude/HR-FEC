77beedb9dbe5d88699cf452d2bf0568f1bbde6a8
Sierra Wendt <serigrace@gmail.com>
2021-07-28 21:42:18 -0700

    merge

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -101,34 +101,34 @@ const App = () => {
     getInitialData();
   }, [])
 
-  // useEffect( () => {
-  //   getInitialData();
-  //   axios.get('http://localhost:3000/products/25171')
-  //   .then( (data) => {
-  //     if (product.id !== data.data.id) {
-  //       console.log(data.data);
-  //       setProduct(data.data);
-  //       return data.data.id;
-  //     } else {
-  //       console.log('same product');
-  //     }
-  //   })
-  //   .then( (id) => {
-  //     axios.get(`http://localhost:3000/products/${id}/styles`)
-  //     .then( (data) => {
-  //       if (styles.product_id !== data.data.product_id) {
-  //         // console.log(data.data);
-  //         setStyles(data.data);
-  //         return data.data.results[0];
-  //       } else {
-  //         console.log('same product/styles');
-  //       }
-  //     })
-  //     .then( (firstStyle) => setStyle(firstStyle) )
-  //     .catch( (e) => console.error(e) );
-  //   })
-  //   .catch( (e) => console.error(e) );
-  //}, []); // An empty array as the second argument of useEffect makes this function run only once on startup, feel free to edit this for your purposes
+  useEffect( () => {
+    getInitialData();
+    axios.get('http://localhost:3000/products/25171')
+    .then( (data) => {
+      if (product.id !== data.data.id) {
+        console.log(data.data);
+        setProduct(data.data);
+        return data.data.id;
+      } else {
+        console.log('same product');
+      }
+    })
+    .then( (id) => {
+      axios.get(`http://localhost:3000/products/${id}/styles`)
+      .then( (data) => {
+        if (styles.product_id !== data.data.product_id) {
+          // console.log(data.data);
+          setStyles(data.data);
+          return data.data.results[0];
+        } else {
+          console.log('same product/styles');
+        }
+      })
+      .then( (firstStyle) => setStyle(firstStyle) )
+      .catch( (e) => console.error(e) );
+    })
+    .catch( (e) => console.error(e) );
+  }, []); // An empty array as the second argument of useEffect makes this function run only once on startup, feel free to edit this for your purposes
 
   return (
     // Our context.Providers 'values' are linked to an object that contains our state hooks.
