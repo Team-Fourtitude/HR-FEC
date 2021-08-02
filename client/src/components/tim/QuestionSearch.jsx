316fa4a4453f1433ba7f1be62b8e7c6fa3da6725
Tim Adeyemo <Timadeyemo@gmail.com>
2021-08-02 19:35:26 -0400

    dry - removed unecessary filter function

--- client/src/components/tim/QuestionSearch.jsx
+++ client/src/components/tim/QuestionSearch.jsx
@@ -9,13 +9,6 @@ const QuestionSearch = () => {
     if (query.length > 1) questionUpdaters.queryQuestions(query)
   }, [query])
 
-  const filterQuestions = (q) => {
-    // Searches only
-    // filterQuestions(e.target.value)
-    setQuery(q);
-    if (q.length > 2 || query.length > 2) questionUpdaters.queryQuestions(q)
-  }
-
   return (
     <div className="question-search">
       <div className="question-search-bar"
