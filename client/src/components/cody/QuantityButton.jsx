e2f5655d775f4e21ea86e2b10e4e436cdade5b9b
Cody Szeto <codyszet@gmail.com>
2021-10-03 22:55:29 -0700

    Initiated a quantity button component

---
+++ client/src/components/cody/QuantityButton.jsx
@@ -0,0 +1,10 @@
+import React, {useState, useContext} from 'react';
+
+const QuantityButton = () => {
+  const currentStyle = useContext(StyleContext);
+  return (
+    <div></div>
+  );
+};
+
+export default QuantityButton;
